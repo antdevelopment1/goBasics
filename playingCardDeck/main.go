b426318dfe2096fdc4eeb5eb06463e3aeea053b0
April Copes <antdevelopment1@gmail.com>
2019-03-22 22:18:16 -0400

    adds for loop example for go syntax

--- playingCardDeck/main.go
+++ playingCardDeck/main.go
@@ -5,8 +5,13 @@ import "fmt"
 func main() {
 	// var card = "Ace of spades"
 	// Can only be used to assign a new variable not to reassign
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{"5 of spades", newCard()}
+	cards = append(cards, "Jack of diamonds")
+	fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 // We have to tell this function what data type it should be expecting to return any time this function is called
