975ae481764fbb5a9a532e5b7699a6ae602fdb37
April Copes <antdevelopment1@gmail.com>
2019-03-22 21:41:58 -0400

    adds function syntax for go

--- playingCardDeck/main.go
+++ playingCardDeck/main.go
@@ -2,16 +2,14 @@ package main
 
 import "fmt"
 
-var deckSize int
-
 func main() {
 	// var card = "Ace of spades"
 	// Can only be used to assign a new variable not to reassign
-	card := "Ace of spades"
+	card := newCard()
 	fmt.Println(card)
-	card = "Five of diamonds"
-	fmt.Println(card)
+}
 
-	deckSize = 52
-	fmt.Println(deckSize)
+// We have to tell this function what data type it should be expecting to return any time this function is called
+func newCard() string {
+	return "Five of diamonds"
 }
